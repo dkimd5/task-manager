02ed774f4d878f5f27d0243fdeca49926e6d143f
dkimd5 <dkimd5@mail.ru>
2022-02-16 09:34:34 +0300

    Add tasks array

--- src/components/CardList/CardList.js
+++ src/components/CardList/CardList.js
@@ -1,6 +1,19 @@
 import React from "react";
 import TaskCard from "./TaskCard";
 
+const initialCards = [
+  { reward: 125, task: "Find dad's wallet", date: today },
+  {
+    reward: 100,
+    task: "Put away old toys to white boxes on the balkoney",
+    date: today,
+  },
+  { reward: 75, task: "Wash the dishes", date: today },
+  { reward: 50, task: "Make your bed in the morning", date: yesterday },
+  { reward: 50, task: "Fix the pillowcase", date: yesterday },
+  { reward: 50, task: "Water plants", date: yesterday },
+];
+
 function CardList() {
   return <div>CardList</div>;
 }
