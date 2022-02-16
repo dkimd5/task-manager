d01184445410891b86f0eb2e3ad20196bbe73815
dkimd5 <dkimd5@mail.ru>
2022-02-16 10:01:51 +0300

    Add card list and task card styles

--- src/components/CardList/CardList.js
+++ src/components/CardList/CardList.js
@@ -1,21 +1,24 @@
 import React from "react";
 import TaskCard from "./TaskCard";
+import "./CardList.css";
 
 const initialCards = [
-  { reward: 125, task: "Find dad's wallet", date: today },
-  {
-    reward: 100,
-    task: "Put away old toys to white boxes on the balkoney",
-    date: today,
-  },
-  { reward: 75, task: "Wash the dishes", date: today },
-  { reward: 50, task: "Make your bed in the morning", date: yesterday },
-  { reward: 50, task: "Fix the pillowcase", date: yesterday },
-  { reward: 50, task: "Water plants", date: yesterday },
+  { reward: 125, task: "Find dad's wallet" },
+  { reward: 100, task: "Put away old toys to white boxes on the balkoney" },
+  { reward: 75, task: "Wash the dishes" },
+  { reward: 50, task: "Make your bed in the morning" },
+  { reward: 50, task: "Fix the pillowcase" },
+  { reward: 50, task: "Water plants" },
 ];
 
 function CardList() {
-  return <div>CardList</div>;
+  return (
+    <ul className="cardslist">
+      {initialCards.map((card, index) => (
+        <TaskCard reward={card.reward} task={card.task} />
+      ))}
+    </ul>
+  );
 }
 
 export default CardList;
--- src/components/CardList/TaskCard/TaskCard.js
+++ src/components/CardList/TaskCard/TaskCard.js
@@ -1,29 +1,27 @@
 import React from "react";
 import "./TaskCard.css";
 
-const cardColor = () => {
-  if (reward >= 10 && reward < 75) {
-    return "lowest-reward";
-  } else if (reward < 100) {
-    return "low-reward";
-  } else if (reward < 125) {
-    return "high-reward";
-  } else if (reward >= 125) {
-    return "highest-reward";
-  } else {
-    return "";
-  }
-};
+function TaskCard({ reward, task }) {
+  const cardColor = () => {
+    if (reward >= 10 && reward < 75) {
+      return "lowest-reward";
+    } else if (reward < 100) {
+      return "low-reward";
+    } else if (reward < 125) {
+      return "high-reward";
+    } else if (reward >= 125) {
+      return "highest-reward";
+    } else {
+      return "";
+    }
+  };
 
-function TaskCard() {
   return (
     <li className={`carditem ${cardColor()}`}>
-      <div className="carditem-front">
-        <div className="carditem-reward-wrp">
-          <span className="carditem-reward">{reward}</span>
-        </div>
-        <p className="carditem-text">{task}</p>
+      <div className="carditem-reward-wrp">
+        <span className="carditem-reward">{reward}</span>
       </div>
+      <p className="carditem-text">{task}</p>
     </li>
   );
 }
