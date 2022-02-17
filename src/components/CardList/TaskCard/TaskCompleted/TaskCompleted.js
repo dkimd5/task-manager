855f7f2a81f3f7fb59038f4fb4b2601d51065be6
dkimd5 <dkimd5@mail.ru>
2022-02-17 10:30:48 +0300

    Add task complete content

--- src/components/CardList/TaskCard/TaskCompleted/TaskCompleted.js
+++ src/components/CardList/TaskCard/TaskCompleted/TaskCompleted.js
@@ -2,7 +2,35 @@ import React from "react";
 import "./TaskCompleted.css";
 
 function TaskCompleted() {
-  return <div>TaskCompleted</div>;
+  return (
+    <div className="cardcomplete">
+      <svg
+        className="cardcomplete-circle-img"
+        width="122"
+        height="122"
+        viewBox="0 0 122 122"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <circle cx="61" cy="61" r="60" stroke="#FF8933" strokeDasharray="5 5" />
+      </svg>
+      <svg
+        className="cardcomplete-welldone-img"
+        width="67"
+        height="51"
+        viewBox="0 0 67 51"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M21.75 40.375L6 24.625L0.75 29.875L21.75 50.875L66.75 5.875L61.5 0.625L21.75 40.375Z"
+          fill="#FF8933"
+        />
+      </svg>
+      <p className="cardcomplete-text">Well done!</p>
+      <p className="cardcomplete-points">You earned {reward} Help Coins! </p>
+    </div>
+  );
 }
 
 export default TaskCompleted;
