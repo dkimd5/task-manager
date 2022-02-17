30688edda834d8cb0f675a05a2e872a024c41a13
dkimd5 <dkimd5@mail.ru>
2022-02-17 13:44:37 +0300

    Add AddTask component content and logic

--- src/components/CardList/TaskCard/AddTask/AddTask.js
+++ src/components/CardList/TaskCard/AddTask/AddTask.js
@@ -3,27 +3,75 @@ import "./AddTask.css";
 import { createMachine } from "xstate";
 import { useMachine } from "@xstate/react";
 
-const addCardMachine = createMachine({
-  id: "addCardMahcine",
-  initial: "addcard",
+const addTaskMachine = createMachine({
+  id: "addTaskMahcine",
+  initial: "addTask",
   states: {
-    addcard: {
+    addtask: {
       on: {
-        START_CARD: "cardoptions",
+        ADD_TASK: "taskoptions",
       },
     },
-    cardoptions: {
+    taskoptions: {
       on: {
-        CANCEL: "addcard",
-        ADD_CARD: "cardadded",
+        CANCEL: "addtask",
+        CREATE_TASK: "addTask",
       },
     },
-    cardadded: {},
+    // taskcreated: {},
   },
 });
 
 function AddTask() {
-  return <div>AddTask</div>;
+  const [current, send] = useMachine(addTaskMachine);
+  const [text, setText] = useState("");
+  const [reward, setReward] = useState("");
+
+  const handleChangeText = (e) => {
+    setText(e.target.value);
+  };
+
+  const handleChangeReward = (e) => {
+    setReward(e.target.value);
+  };
+
+  return (
+    <>
+      {current.matches("addtask") && (
+        <li
+          className="addtask"
+          onClick={() => {
+            send("ADD_TASK");
+          }}
+        ></li>
+      )}
+      {current.matches("taskoptions") && (
+        <li className="task-options">
+          <div className="task-options-wrp">
+            <h3>New housework task</h3>
+            <form>
+              <label htmlFor="taskText">Title</label>
+              <input
+                value={text}
+                id="taskText"
+                type="text"
+                onChange={handleChangeText}
+              ></input>
+              <label htmlFor="taskReward">Reward</label>
+              <input
+                value={reward}
+                id="taskReward"
+                type="text"
+                onChange={handleChangeReward}
+              ></input>
+            </form>
+          </div>
+          <button onClick={() => send("CANCEL")}>Cancel</button>
+          <button onClick={() => send("CREATE_TASK")}>Create</button>
+        </li>
+      )}
+    </>
+  );
 }
 
 export default AddTask;
