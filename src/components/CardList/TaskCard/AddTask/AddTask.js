4314e5e80eb3b5d37632dc1b12b5c2f080e6f16c
dkimd5 <dkimd5@mail.ru>
2022-02-17 13:32:25 +0300

    Add xstate machine for add task

--- src/components/CardList/TaskCard/AddTask/AddTask.js
+++ src/components/CardList/TaskCard/AddTask/AddTask.js
@@ -1,5 +1,26 @@
 import React from "react";
 import "./AddTask.css";
+import { createMachine } from "xstate";
+import { useMachine } from "@xstate/react";
+
+const addCardMachine = createMachine({
+  id: "addCardMahcine",
+  initial: "addcard",
+  states: {
+    addcard: {
+      on: {
+        START_CARD: "cardoptions",
+      },
+    },
+    cardoptions: {
+      on: {
+        CANCEL: "addcard",
+        ADD_CARD: "cardadded",
+      },
+    },
+    cardadded: {},
+  },
+});
 
 function AddTask() {
   return <div>AddTask</div>;
