e0d0f24c7e5ea627c432ade09aaf01bf89e6f675
dkimd5 <dkimd5@mail.ru>
2022-02-18 10:47:56 +0300

    Add router to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import CardList from "./components/CardList";
 
 function App() {
   return (
     <div className="App">
-      <CardList />
+      <BrowserRouter>
+        <ul>
+          <li>
+            <Link to="/task-list">Today's housework</Link>
+          </li>
+          <li>
+            <Link to="/task-history">History</Link>
+          </li>
+        </ul>
+
+        <Routes>
+          <Route path="/" exact element={<CardList />} />
+          <Route path="/:id" element={<CardList />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
