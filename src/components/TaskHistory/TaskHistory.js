50b1676ee5b79b0ed6b1d9616d2da43d0146d3c7
dkimd5 <dkimd5@mail.ru>
2022-02-19 11:23:49 +0300

    Add card history render

--- src/components/TaskHistory/TaskHistory.js
+++ src/components/TaskHistory/TaskHistory.js
@@ -1,8 +1,32 @@
 import React from "react";
+import CardFrontside from "../CardList/TaskCard/CardFrontside";
 import "./TaskHistory.css";
+import { TODAY } from "../../utils/variables";
 
-function TaskHistory() {
-  return <div>TaskHistory</div>;
+function TaskHistory({ cards }) {
+  console.log(cards);
+
+  return (
+    <div className="history">
+      {cards.map((day) => (
+        <div className="history-day" key={day.id}>
+          <span className="history-day-date">
+            {TODAY === day.id ? <span>Today</span> : ""}
+            {day.id}
+          </span>
+          <ul className="history-card-list">
+            {Object.values(day)
+              .splice(-0, Object.values(day).length - 1)
+              .map((card) => (
+                <li className="carditem">
+                  <CardFrontside card={card} />
+                </li>
+              ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default TaskHistory;
